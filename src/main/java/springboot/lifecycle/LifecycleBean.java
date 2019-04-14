bf1aee9890eef576d7168904f0277c3b29b292c8
Adam Ostrožlík <adamostrozlik@yandex.com>
2019-04-14 19:48:35 +0200

    JPA

--- src/main/java/springboot/lifecycle/LifecycleBean.java
+++ src/main/java/springboot/lifecycle/LifecycleBean.java
@@ -1,13 +1,16 @@
 package springboot.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component
 public class LifecycleBean implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
--- src/main/java/springboot/recipe/repositories/IngredientRepository.java
+++ src/main/java/springboot/recipe/repositories/IngredientRepository.java
@@ -1,7 +1,6 @@
 package springboot.recipe.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import springboot.recipe.models.Description;
 import springboot.recipe.models.Ingredient;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
